57e02e249b7117c1ef59c74925957e4e3ca71962
Samy Pessé <samypesse@gmail.com>
2014-03-31 18:37:49 -0700

    Add base gitbook task

---
+++ tasks/gitbook.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+    var fs = require('fs'),
+            gitbook = require('gitbook'),
+            path = require('path');
+
+    grunt.registerMultiTask('gitbook', 'gitbook builder', function() {
+        var config = this.data;
+        var done = this.async();
+
+        if (!config.dest || !config.input || !config.title || !config.github) {
+            return done(new Error("Need options 'dest', 'input', 'title' and 'github'"));
+        }
+        
+        gitbook.generate.folder(
+            config.input,
+            config.dest,
+            {
+                title: config.title,
+                github: config.github
+            }
+        )
+        .then(function() {
+            done();
+        }, done);
+    });
+};
